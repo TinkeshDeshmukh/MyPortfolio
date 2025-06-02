8ac7165d5f574209a1e03991ec86522423ac804d
TinkeshDeshmukh <149570723+TinkeshDeshmukh@users.noreply.github.com>
2025-06-02 20:15:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter , Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
 
 function App() {
   return (
     <div>
-      <HashRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </div>
+      <HashRouter >
+      <Routes>
+      
+        <Route index element ={<Home/>}/>
+        <Route path="*" element={<NotFound />} />
+        
+        {/* Add more routes here as needed */}
+
+      </Routes>
+      </HashRouter >
+    </div>  
   )
 }
 
